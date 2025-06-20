b2538df6eba373a58e1116551e5694540f1b7151
riofadil9 <riofadil9@gmail.com>
2025-06-20 17:01:36 +0700

    jalan yg dideteksi, kuning sama coklat

--- src/config.py
+++ src/config.py
@@ -32,18 +32,29 @@ SIMULATOR_PORT = 25001
 # Setting untuk trackbar warna putih dalam format BGR
 # Format: ("nama", nilai_default, nilai_maksimum)
 WHITE_SETTING = [
-    ("b_low", 0, 255), ("g_low", 0, 255), ("r_low", 0, 255),
-    ("b_high", 65, 255), ("g_high", 106, 255), ("r_high", 255, 255)
+    ("b_low", 0, 255), ("g_low", 0, 255), ("r_low", 102, 255),
+    ("b_high", 104, 255), ("g_high", 148, 255), ("r_high", 180, 255)
 ]
 
 # Setting untuk trackbar warna kuning dalam format BGR
 YELLOW_SETTING = [
-    ("b_low", 0, 255), ("g_low", 150, 255), ("r_low", 150, 255),
-    ("b_high", 100, 255), ("g_high", 255, 255), ("r_high", 255, 255)
+    ("b_low", 0, 255), ("g_low", 53, 255), ("r_low", 91, 255),
+    ("b_high", 98, 255), ("g_high", 183, 255), ("r_high", 206, 255)
 ]
 
-# Setting untuk trackbar warna tambahan (misal: Biru) dalam format BGR
-ADDITIONAL_COLOR_SETTING = [
-    ("b_low", 150, 255), ("g_low", 0, 255), ("r_low", 0, 255),
-    ("b_high", 255, 255), ("g_high", 100, 255), ("r_high", 100, 255)
-]
+# # Setting untuk trackbar warna tambahan (misal: Biru) dalam format BGR
+# ADDITIONAL_COLOR_SETTING = [
+#     ("b_low", 150, 255), ("g_low", 0, 255), ("r_low", 0, 255),
+#     ("b_high", 255, 255), ("g_high", 100, 255), ("r_high", 100, 255)
+# ]
+
+#HSL
+# WHITE_SETTING = [
+#     ("h_low", 0, 180), ("l_low", 200, 255), ("s_low", 0, 255),
+#     ("h_high", 180, 180), ("l_high", 255, 255), ("s_high", 255, 255)
+# ]
+
+# YELLOW_SETTING = [
+#     ("h_low", 15, 180), ("l_low", 30, 255), ("s_low", 100, 255),
+#     ("h_high", 35, 180), ("l_high", 204, 255), ("s_high", 255, 255)
+# ]
